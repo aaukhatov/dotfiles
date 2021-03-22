61138c9c2cf876494f4d9b6e6b985a9c7108ab2c
Artur Aukhatov <aaukhatov@gmail.com>
2021-03-22 22:13:35 +0300

    Update GIT configs

--- git.sh
+++ git.sh
@@ -3,3 +3,5 @@
 git config --global user.name "Artur Aukhatov"
 git config --global user.email "aaukhatov@gmail.com"
 git config --global color.ui true
+git config --global color.diff auto
+git config --global core.editor vim
