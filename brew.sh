3676280c90946be66bd19e532a3994e25ea3a8fe
Artur Aukhatov <aaukhatov@gmail.com>
2021-03-22 22:07:57 +0300

    Add brew's packages install script

---
+++ brew.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+brew update
+brew upgrade
+
+brew install git
+brew install vim
+brew install jq
+brew install bash-completion
+brew install nmap
+brew install findutils
+brew install htop
+brew install openssl
+brew install tmux
+brew install zsh
+brew install zsh-syntax-highlighting
+brew install wget
+brew install tree
+brew install utf8proc
+brew install gcc
+brew install gdbm
